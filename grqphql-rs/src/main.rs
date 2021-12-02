161b9f15effbf3c8873ad2ad39cf0ddbafe64904
Viki Val <viktorko99@gmail.com>
2021-12-02 22:26:33 +0100

    02.12 - :zap: functional graphql parser

--- grqphql-rs/src/main.rs
+++ grqphql-rs/src/main.rs
@@ -1,7 +1,18 @@
 use reqwest::Client;
 use graphql_client::{GraphQLQuery, Response};
+use regex::Regex;
 
 const GRAPHQL_API: &str = "https://gql.rmrk.app/v1/graphql";
+
+#[derive(Debug)]
+pub struct NFT {
+    id: String,
+    metadata: String,
+    metadata_image: String,
+    collection_id: String,
+}
+
+type NFTs = Vec<NFT>;
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -9,7 +20,9 @@ const GRAPHQL_API: &str = "https://gql.rmrk.app/v1/graphql";
     query_path = "query.graphql",
     response_derives = "Debug",
 )]
-pub struct AllTokens;
+pub struct AllTokens {
+    pub nfts: NFTs,
+}
 
 #[tokio::main]
 async fn main() {
@@ -27,7 +40,25 @@ async fn main() {
         .await
         .expect("failed to parse json");
 
-    
-    //  let response_body: Response<union_query::ResponseData> = res.json().await?;
-    //  println!("{:#?}", response_body);
+    //  println!("{:#?}", response);
+    let data = response.data.expect("data is null");
+    let re = Regex::new(r"^[a-z0-9]+").unwrap();
+    let nfts: Vec<NFT> = data.nfts.iter().map(|nft| NFT {
+        id: nft.id.clone(),
+        metadata: unpack(&nft.metadata),
+        metadata_image: unpack(&nft.metadata_image),
+        collection_id: nft.collection_id.clone(),
+    })
+    .filter(|nft| re.is_match(&nft.collection_id))
+    .collect();
+
+     println!("{:#?}", nfts);
+}
+
+
+fn unpack(s: &Option<String>) -> String {
+    match s {
+        Some(s) => s.clone(),
+        None => "".to_string(),
+    }
 }
